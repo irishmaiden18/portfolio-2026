44c7f4e543632aaf575b46b999e88e0ca305b0c8
irishmaiden18 <laurapohl18@gmail.com>
2026-05-19 18:33:16 -0400

    removed bootstrap

--- src/components/DuringDevelopment.jsx
+++ src/components/DuringDevelopment.jsx
@@ -1,14 +1,13 @@
-import '../bootstrap-isolate.css'
 
 const DuringDevelopment = () => {
     return(
         <>
-            <div className="d-block d-sm-none fa-2x">XS</div>
-            <div className="d-none d-sm-block d-md-none fa-2x">S</div>
-            <div className="d-none d-md-block d-lg-none fa-2x">M</div>
-            <div className="d-none d-lg-block d-xl-none fa-2x">L</div>
-            <div className="d-none d-xl-block d-xxl-none fa-2x">XL</div>
-            <div className="d-none d-xxl-block fa-2x">XXL</div>
+            <div className="block sm:hidden text-4xl">XS</div>
+            <div className="hidden sm:block md:hidden text-4xl">S</div>
+            <div className="hidden md:block lg:hidden text-4xl">M</div>
+            <div className="hidden lg:block xl:hidden text-4xl">L</div>
+            <div className="hidden xl:block 2xl:hidden text-4xl">XL</div>
+            <div className="hidden 2xl:block text-4xl">XXL</div>
             {/* Note: remove bootstrap immport from main.jsx, this file and uninstiall bootstrap with "npm uninstall bootstrap" */}
         </>
     )

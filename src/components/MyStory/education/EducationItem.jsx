574fa8505812e51b4ff717b7646cec26244ff6d7
irishmaiden18 <laurapohl18@gmail.com>
2026-07-30 11:47:10 -0400

    adds comma to education list

--- src/components/MyStory/education/EducationItem.jsx
+++ src/components/MyStory/education/EducationItem.jsx
@@ -7,7 +7,7 @@ const EducationItem = ({education}) => {
     <li className="text-center md:text-left pb-4 md:pb-0 lg:pb-4">
         <span className="font-bold">{subject}</span><span className="hidden md:inline">, </span>
         <br className="md:hidden"/>
-        <span className="">{degree_type}{honors && ", "}</span><span className="italic tracking-wide text-neutral-700">{honors && `${honors} `}</span>
+        <span className="">{degree_type}, </span><span className="italic tracking-wide text-neutral-700">{honors && `${honors}, `}</span>
         <br className="md:hidden lg:block xl:hidden"/>
         <span>{institution}</span>
     </li>

07bf5a393e7a79f88d5ebe9357833072af5477c4
irishmaiden18 <laurapohl18@gmail.com>
2026-08-15 20:45:05 -0400

    deletes semicolons

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,8 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router'
 
 // header font import
-import "@fontsource/tangerine";
-import "@fontsource/tangerine/700.css"; 
+import "@fontsource/tangerine"
+import "@fontsource/tangerine/700.css" 
 
 
 createRoot(document.getElementById('root')).render(

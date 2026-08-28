3955896c2e50e868c2774a43dcdfbb6d5d859063
irishmaiden18 <laurapohl18@gmail.com>
2026-08-28 15:39:08 -0400

    switches project order

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,12 +1,12 @@
 export const projects = [
     {
         id: 0,
-        name: "All About Dogs Website",
-        image: "all_about_dogs_website.jpg",
-        description: "A responsive, multi-feature dog encyclopedia website. The website integrates multiple external REST APIs that fetch data, implements dynamic DOM manipulation to render a random photo grid that adjusts the number and width of displayed images based on screen size, displays a random dog facts list and a detailed breed info panel that is populated based on user selected breed from an interactive dropdown populated from 1,000+ breeds from the external APIs.",
-        technology: "HTML, CSS, JavaScript, & REST API",
-        site: "https://irishmaiden18.github.io/All-About-Dogs-Fetch-Mini-Project/",
-        github: "https://github.com/irishmaiden18/All-About-Dogs-Fetch-Mini-Project"
+        name: "MiniShop",
+        image: "minishop.jpg",
+        description: "A responsive sample e-commerce web application that displays products from a remote API with filtering using buttons and a search field. It uses Auth0 for user authentication where logged-in users can add products and view an order history and features Paypal payment integration at checkout.",
+        technology: "React, JavaScript, HTML, CSS, Browser Router, Auth0, PayPal, Tailwind, REST API",
+        site: "https://minishop-oo89.onrender.com",
+        github: "https://github.com/irishmaiden18/MiniShop"
     },
     {
         id: 1,
@@ -19,13 +19,14 @@ export const projects = [
     },
     {
         id: 2,
-        name: "MiniShop",
-        image: "minishop.jpg",
-        description: "A responsive sample e-commerce web application that displays products from a remote API with filtering using buttons and a search field. It uses Auth0 for user authentication where logged-in users can add products and view an order history and features Paypal payment integration at checkout.",
-        technology: "React, JavaScript, HTML, CSS, Browser Router, Auth0, PayPal, Tailwind, REST API",
-        site: "https://minishop-oo89.onrender.com",
-        github: "https://github.com/irishmaiden18/MiniShop"
+        name: "All About Dogs Website",
+        image: "all_about_dogs_website.jpg",
+        description: "A responsive, multi-feature dog encyclopedia website. The website integrates multiple external REST APIs that fetch data, implements dynamic DOM manipulation to render a random photo grid that adjusts the number and width of displayed images based on screen size, displays a random dog facts list and a detailed breed info panel that is populated based on user selected breed from an interactive dropdown populated from 1,000+ breeds from the external APIs.",
+        technology: "HTML, CSS, JavaScript, & REST API",
+        site: "https://irishmaiden18.github.io/All-About-Dogs-Fetch-Mini-Project/",
+        github: "https://github.com/irishmaiden18/All-About-Dogs-Fetch-Mini-Project"
     },
+    
     // {
     //     id: 2,
     //     image: "coming_soon.jpg",
